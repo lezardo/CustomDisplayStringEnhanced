af137a163a4b08446ac2c0aa3a3dfd5bf3b55985
nevakee716 <nevakee716@gmail.com>
2018-04-16 18:58:36 +0200

    init

---
+++ src/cdsEnhanced.js
@@ -0,0 +1,63 @@
+ /*jslint browser:true*/
+/*global cwAPI, jQuery, cwTabManager*/
+(function(cwApi, $) {
+  'use strict';
+
+
+var cdsEnhanced = {};
+
+cdsEnhanced.getDisplayItem = function(item, nameOnly) {
+    var itemDisplayName, titleOnMouseOver, link, itemLabel, markedForDeletion, linkTag, linkEndTag,popOutInfo,popOutSplit,popOutName,popOutText,popoutElement;
+
+
+    // use the display property scriptname
+    itemLabel = this.displayProperty.getDisplayString(item);
+    link = !cwApi.isUndefined(this.defaultLinkView) ? this.singleLinkMethod(this.defaultLinkView, item) : "";
+    titleOnMouseOver = this.hasTooltip && !cwApi.isUndefined(item.properties.description) ? cwApi.cwEditProperties.cwEditPropertyMemo.isHTMLContent(item.properties.description) ? $(item.properties.description).text() : item.properties.description : "";
+
+    markedForDeletion = cwApi.isObjectMarkedForDeletion(item) ? ' markedForDeletion' : '';
+    if (this.options.HasLink === false) {
+        if (itemLabel.indexOf('<@') !== -1 && itemLabel.indexOf('\\<@') === -1) {
+            itemLabel = itemLabel.replace(/<@/g, '').replace(/@>/g, '');
+        }
+        itemDisplayName = "<span class='" + this.nodeID + markedForDeletion + "' title=\"" + titleOnMouseOver + "\">" + itemLabel + "</span>";
+    } else {
+        linkTag = "<a class='" + this.nodeID + markedForDeletion + "' href='" + link + "'>";
+        linkEndTag = "</a>";
+        if (itemLabel.indexOf('<@') !== -1 && itemLabel.indexOf('\\<@') === -1) {
+            itemDisplayName = itemLabel.replace(/<@/g, linkTag).replace(/@>/g, linkEndTag);
+        } else {
+            itemDisplayName = linkTag + itemLabel + linkEndTag;
+        }
+    }
+
+    if(itemDisplayName.indexOf('<#') !== -1 && itemDisplayName.indexOf('#>') !== -1) {
+        popOutInfo = itemDisplayName.split("<#")[1].split("#>")[0];
+        popOutSplit = popOutInfo.split("#");
+        popOutName = popOutSplit[1];
+        popOutText = popOutSplit[0];
+        popoutElement = '<span onclick="cwAPI.customFunction.openDiagramPopoutWithID(' + item.object_id + ',\'' + popOutName + '\');">' + popOutText + "</span>";
+        itemDisplayName = itemDisplayName.replace('<#' + popOutInfo + '#>',popoutElement);
+    }
+
+
+    $('span').attr('data-children-number');
+
+    if (!cwApi.isUndefined(nameOnly) && nameOnly === true) {
+        itemDisplayName = "<label class='" + this.nodeID + "'>" + itemLabel + "</label>";
+    }
+    return itemDisplayName;
+};
+
+if(cwAPI.customFunction === undefined) cwAPI.customFunction = {};
+cwApi.customFunction.openDiagramPopoutWithID = function(id,popOutName) {
+    var obj = {};
+    obj.object_id = id;
+    cwAPI.cwDiagramPopoutHelper.openDiagramPopout(obj,popOutName);
+
+};
+
+cwApi.cwLayouts.CwLayout.prototype.getDisplayItem = cdsEnhanced.getDisplayItem;
+
+
+}(cwAPI, jQuery));
